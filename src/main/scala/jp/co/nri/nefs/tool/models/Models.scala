9f53161527405ff1705c04a3b2b7babf7106701b
s7.nakamura <s7.nakamura@gmail.com>
2019-03-21 16:17:01 +0900

    logger導入

--- src/main/scala/jp/co/nri/nefs/tool/models/Models.scala
+++ src/main/scala/jp/co/nri/nefs/tool/models/Models.scala
@@ -2,6 +2,7 @@ package jp.co.nri.nefs.tool.models
 
 import java.util.Date
 
-case class Dialog(dialogName: String, handler: Option[String], destinationType: Option[String], userName: String, tradeDate: String, time: Date, startupTime: Long)
+case class Dialog(handler: String, dialogName: Option[String], action: Option[String],
+                  destinationType: Option[String], userName: String, tradeDate: String, time: Date, startupTime: Long)
 
 //class Dialogs(tag: Tag) extends
